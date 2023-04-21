2794ea6b43e2c08abf13d9c840779435ac4e936a
Zwoooz <benjamin@solbergarvidsson.se>
2023-04-21 16:12:23 +0200

    button testing 2

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,6 +5,7 @@ const queue = require('../commands/queue');
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction) {
+    console.log(interaction);
     if(interaction.customId) {
       if(!queue.setFields) return;
       if(typeof listNumber === 'undefined') listNumber = 0;
